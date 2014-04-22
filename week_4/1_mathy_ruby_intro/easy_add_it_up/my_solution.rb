271a4ccf24d7eb3b716b2f30e08fc4bd9c141ece
Martin Lear <m.lear09@gmail.com>
2014-04-21 23:24:26 -0500

    Exercise 1

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,22 +4,59 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Zach Pflederer].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+
+# 1.) We will need to define a method as total.
+# 2.) We will pass one argument to the method.
+# 3.) The argument will be an array of numbers.
+# 4.) The method will return the sum of the numbers in the argument array.
+
+# 5.) We will define a method as sentence_maker.
+# 6.) We will pass a single argument to the sentence_maker method.
+# 7.) The argument will be an array of strings.
+# 8.) The method will iterate through the array that is passed to it and join the separate elements into a single string.
+# 9.) We will properly punctuate the result.
+# 10.) The method will return a grammatically correct sentence based on the array provided.
+
 
 
 # 2. Initial Solution
 
+def total(numbers)
+    sum = 0
+    numbers.each { |x| sum = (sum + x) }
+    sum
+end
 
-
+def sentence_maker(words)
+	words.map! { |x| x.class == String ? x : x.to_s }
+    words[0].capitalize!
+    words[-1] = "#{words[-1]}."
+    words[1..-1].map! { |x| x.downcase }
+    words.join(" ")
+end
 
 # 3. Refactored Solution
 
+def total(numbers)
+    sum = 0
+    numbers.each { |x| sum = (sum + x) }
+    sum
+end
+
+def sentence_maker(words)
+    words.map! { |x| x.class == String ? x : x.to_s }
+    words.map.downcase! words.map.downcase!  #We thought this was more readable as code.  It spelled out the steps more clearly.
+    words[0].capitalize!
+    words[-1] = "#{words[-1]}."
+    words.join(" ")
+end
 
 
-# 4. Reflection 
+# 4. Reflection - This was tough getting back into ruby after focusing on html, css and java for
+# the last few weeks.  We had a couple of miscures working through the code, but figured it
+# out quick enough.  I almost feel like this should have come to reinforce what we had learned right
+# at the beginning before html and css.
--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -4,22 +4,51 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
-
+# 1.) I will define method as get_grade.
+# 2.) get_grade will be passed an array of numbers and an average will be calculated
+# 2.) When get_grade is passed an integer in the value or 90 or greater, it will return
+# 		the grade of "A"
+# 3.) When get_grade is passed an integer in the value of 80 or greater, it will return
+# 		the grade of "B" 
+# 4.) When get_grade is passed an integer in the value of 70 or greater, it will return
+# 		the grade of "C" 
+# 5.) When get_grade is passed an integer in the value of 60 or greater, it will return
+# 		the grade of "D" 
+# 6.) When get_grade is passed an integer in the value of 50 or greater, it will return
+# 		the grade of "F" 
 
 # 2. Initial Solution
 
+def get_grade(grades)
+	sum = 0
+	total = grades.length
+	grades.each { |x| sum = (sum + x) }
+	average = (sum / total)
+	if average >= 90
+		return "A"
+	elsif average >= 80
+		return "B"
+	elsif average >= 70
+		return "C"
+	elsif average >= 60
+		return "D"
+	else 
+		return "F"
+	end
+end
 
 
 
 # 3. Refactored Solution
+# I don't know if this can be made any simpler with what I currently know.
 
 
-
-# 4. Reflection 
+# 4. Reflection - I made the huge mistake of trying to call the .each on get_grade instead of 
+# grades for a good 15 minutes.  That was frustration.  I finally realized what I was doing
+# and it passed all the tests right away.  It was a good learning experience and gives me
+# something to remember to look for in the furture.  This exercise was almost easier to me in total
+# then the easy exercise was.
