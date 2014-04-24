1ad5af86f69acc0c265e74baa01700e6e151e45c
Martin Lear <m.lear09@gmail.com>
2014-04-23 23:43:02 -0500

    Release 5

--- week_4/6_cipher_challenge/my_solution.rb
+++ week_4/6_cipher_challenge/my_solution.rb
@@ -38,82 +38,6 @@ def north_korean_cipher(coded_message) #creating a new method called north_korea
   input.each do |x| # What is #each doing here?
   #iterates over the letters in the input array
     found_match = false  # Why would this be assigned to false from the outset? What happens when it's true?
-    #setting 'found_match' as a boolean to false
-    cipher.each_key do |y| # What is #each_key doing here?
-      if x == y  # What is this comparing? Where is it getting x? Where is it getting y? What are those variables really?
-        # puts "I am comparing x and y. X is #{x} and Y is #{y}."
-        decoded_sentence << cipher[y]
-        found_match = true
-        break  # Why is it breaking here?
-      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #What the heck is this doing?
-        decoded_sentence << " "
-        found_match = true
-        break
-      elsif (0..9).to_a.include?(x) # Try this out in IRB. What does   " (0..9).to_a "    do?
-        decoded_sentence << x
-        found_match = true
-        break
-      end 
-    end
-    if not found_match  # What is this looking for?
-      decoded_sentence << x
-    end
-  end
-  decoded_sentence = decoded_sentence.join("")
- 
-  if decoded_sentence.match(/\d+/) #What is this matching? Look at Rubular for help. 
-    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } #He's been known to exaggerate...
-  end  
-  return decoded_sentence # What is this returning?        
-end
-
-# Your Refactored Solution
-
-
-
-
-
-# Driver Code:
-p "test 1"# 1. Solution
-# Write your comments on what each thing is doing. 
-# If you have difficulty, go into IRB and play with the methods.
-
-
-
-def north_korean_cipher(coded_message) #creating a new method called north_korean_cipher, and passing into it a coded message
-  input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the ruby docs.
-  #this creates a new array called 'input' wherein each member is one letter from the coded message
-  decoded_sentence = [] #creates a new empty array, presumably a place to put de-coded letters
-  cipher = {"e" => "a",   # This is technically a shift of four letters...Can you think of a way to automate this? Is a hash
-            "f" => "b",   # the best data structure for this problem? What are the pros and cons of hashes?
-            "g" => "c",   # Creates a hash that decodes individual letters of the alphabet
-            "h" => "d", 
-            "i" => "e", 
-            "j" => "f",
-            "k" => "g",
-            "l" => "h",
-            "m" => "i",
-            "n" => "j",
-            "o" => "k",
-            "p" => "l",
-            "q" => "m",
-            "r" => "n",
-            "s" => "o",
-            "t" => "p",
-            "u" => "q",
-            "v" => "r",
-            "w" => "s",
-            "x" => "t",
-            "y" => "u",
-            "z" => "v",
-            "a" => "w",
-            "b" => "x",
-            "c" => "y",
-            "d" => "z"}
-            
-  input.each do |x| # What is #each doing here?
-  #iterates over the letters in the input array
-    found_match = false  # Why would this be assigned to false from the outset? What happens when it's true?
     #setting 'found_match' as a boolean to false, so if 'x' doesn't match anything in the following each loop, it will get shovelled into the decoded message (see below)
     cipher.each_key do |y| # What is #each_key doing here? 
     #iterating x over keys in cipher hash
@@ -153,104 +77,32 @@ def north_korean_cipher(coded_message) #creating a new method called north_korea
 end # THE END
 
 # Your Refactored Solution
-
 def north_korean_cipher(coded_message)
-  input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the ruby docs.
-  decoded_sentence = []
-  cipher = {"e" => "a",   # This is technically a shift of four letters...Can you think of a way to automate this? Is a hash
-            "f" => "b",   # the best data structure for this problem? What are the pros and cons of hashes?
-            "g" => "c", 
-            "h" => "d", 
-            "i" => "e", 
-            "j" => "f",
-            "k" => "g",
-            "l" => "h",
-            "m" => "i",
-            "n" => "j",
-            "o" => "k",
-            "p" => "l",
-            "q" => "m",
-            "r" => "n",
-            "s" => "o",
-            "t" => "p",
-            "u" => "q",
-            "v" => "r",
-            "w" => "s",
-            "x" => "t",
-            "y" => "u",
-            "z" => "v",
-            "a" => "w",
-            "b" => "x",
-            "c" => "y",
-            "d" => "z"}
-            
-  input.each do |x| # What is #each doing here?
-    found_match = false  # Why would this be assigned to false from the outset? What happens when it's true?
-    cipher.each_key do |y| # What is #each_key doing here?
-      if x == y  # What is this comparing? Where is it getting x? Where is it getting y? What are those variables really?
-        #puts "I am comparing x and y. X is #{x} and Y is #{y}."
-        decoded_sentence << cipher[y]
-        found_match = true
-        break  # Why is it breaking here?
-      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #What the heck is this doing?
+    input = coded_message.downcase.split("")
+    decoded_sentence = []
+    alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    input.each do |x|
+      if alpha.include?(x)
+        cipher_index = alpha.index(x) - 4
+        decoded_sentence << alpha[cipher_index]
+      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*"
         decoded_sentence << " "
-        found_match = true
-        break
-      elsif (0..9).to_a.include?(x) # Try this out in IRB. What does   " (0..9).to_a "    do?
+      elsif (0..9).to_a.include?(x) 
         decoded_sentence << x
-        found_match = true
-        break
-      end 
+      else
+        decoded_sentence << x
+      end
+      
     end
-    if found_match == false  # What is this looking for?
-      decoded_sentence << x
-    end
-  end
-  decoded_sentence = decoded_sentence.join("")
- p decoded_sentence
-  if decoded_sentence.match(/\d+/) #What is this matching? Look at Rubular for help. 
-    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } #He's been known to exaggerate...
-  end  
-  return decoded_sentence # What is this returning?        
+    decoded_sentence = decoded_sentence.join("")
+    if decoded_sentence.match(/\d+/) #What is this matching? Look at Rubular for help. 
+      decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } #He's been known to exaggerate...
+    end  
+  return decoded_sentence # What is this returning?    
 end
-
-
 
 # Driver Code:
 p "test 1"
-p north_korean_cipher("m^aerx%e&gsoi!") == "i want a coke!" #This is driver code and should print true
- if north_korean_cipher("m^aerx%e&gsoi!") != "i want a coke!"
-     p "Error: Cipher should equal \'i want a coke!\'"
- end
-# Find out what Kim Jong Un is saying below and turn it into driver code as well. Driver Code statements should always return "true"
-p "test 2"
-p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.") == "our people eat the most delicious and nutritious foods from our 10000 profitable farms."
-    if north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.") != "our people eat the most delicious and nutritious foods from our 10000 profitable farms."
-     p "Error: Cipher should equal \'our people eat the most delicious and nutritious foods from our 10000 profitable farms.\'"
- end
-p "test 3"
-p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!") == "our nukes are held together by grape-flavored toffee. don't tell the us!"
-    if north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!") != "our nukes are held together by grape-flavored toffee. don't tell the us!"
-     p "Error: Cipher should equal \'our nukes are held together by grape-flavored toffee. don't tell the us!\'"
- end
-
-p "test 4"
-p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.") == "if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman."
-    if north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.") != "if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman."
-     p "Error: Cipher should equal \'if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman.\'"
- end
-p "test 5"
-p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!") == "next stop: south korea, then japan, then the world!"
-    if north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!") != "next stop: south korea, then japan, then the world!"
-     p "Error: Cipher should equal \'next stop: south korea, then japan, then the world!\'"
- end
-p "test 6"
-p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?") == "can somebody just get me 100 bags of cool ranch doritos?"
-    if north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?") != "can somebody just get me 100 bags of cool ranch doritos?"
-     p "Error: Cipher should equal \'can somebody just get me 100 bags of cool ranch doritos?\'"
- end
-# Reflection
- 
 p north_korean_cipher("m^aerx%e&gsoi!") == "i want a coke!" #This is driver code and should print true
  if north_korean_cipher("m^aerx%e&gsoi!") != "i want a coke!"
      p "Error: Cipher should equal \'i want a coke!\'"
