0dae65626d348b631801f3023d8803171ef92fc7
Martin Lear <m.lear09@gmail.com>
2014-04-23 21:31:48 -0500

    Release 1

--- week_4/6_cipher_challenge/my_solution.rb
+++ week_4/6_cipher_challenge/my_solution.rb
@@ -1,10 +1,3 @@
-# U2.W4: Cipher Challenge
-
-
-# I worked on this challenge with: .
-
-
-
 # 1. Solution
 # Write your comments on what each thing is doing. 
 # If you have difficulty, go into IRB and play with the methods.
@@ -79,12 +72,29 @@ end
 
 # Driver Code:
 p north_korean_cipher("m^aerx%e&gsoi!") == "i want a coke!" #This is driver code and should print true
+ if north_korean_cipher("m^aerx%e&gsoi!") != "i want a coke!"
+     p "Error: Cipher should equal \'i want a coke!\'"
+ end
 # Find out what Kim Jong Un is saying below and turn it into driver code as well. Driver Code statements should always return "true"
-p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.")
-p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!")
-p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.")
-p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!")
-p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?")
-
+p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.") == "our people eat the most delicious and nutritious foods from our 10000 profitable farms."
+    if north_korean_cipher("m^aerx%e&gsoi!") != "our people eat the most delicious and nutritious foods from our 10000 profitable farms!"
+     p "Error: Cipher should equal \'our people eat the most delicious and nutritious foods from our 10000 profitable farms.\'"
+ end
+p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!") == "our nukes are held together by grape-flavored toffee. don't tell the us!"
+    if north_korean_cipher("m^aerx%e&gsoi!") != "our nukes are held together by grape-flavored toffee. don't tell the us!"
+     p "Error: Cipher should equal \'our nukes are held together by grape-flavored toffee. don't tell the us!\'"
+ end
+p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.") == "if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman."
+    if north_korean_cipher("m^aerx%e&gsoi!") != "if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman."
+     p "Error: Cipher should equal \'if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman.\'"
+ end
+p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!") == "next stop: south korea, then japan, then the world!"
+    if north_korean_cipher("m^aerx%e&gsoi!") != "next stop: south korea, then japan, then the world!"
+     p "Error: Cipher should equal \'next stop: south korea, then japan, then the world!\'"
+ end
+p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?") == "can somebody just get me 100 bags of cool ranch doritos?"
+    if north_korean_cipher("m^aerx%e&gsoi!") != "can somebody just get me 100 bags of cool ranch doritos?"
+     p "Error: Cipher should equal \'can somebody just get me 100 bags of cool ranch doritos?\'"
+ end
 # Reflection
  
