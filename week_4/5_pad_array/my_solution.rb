c01a26d78c3202a988320f6d1d0f5bc6d5cdb0ed
Martin Lear <m.lear09@gmail.com>
2014-04-26 16:04:09 -0500

    Updates

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -8,18 +8,36 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? The input will be a maximum element count for an array, along with an optional value to add to the array.
+# What is the output? (i.e. What should the code return?)  Depending on which method is run, either a new array or the original array modified.
 # What are the steps needed to solve the problem?
+#
+# Define 2 methods, one called pad, that creates a new array and pads it.  The other pad! that will modify the array being passed
+# into the method.
 
 
 # 2. Initial Solution
+class Array
+	def pad(size, value = nil)
+   		new_array = self.dup
+   		new_array[(size - new_array.length).times { new_array.push value }]
+   		return new_array
+	end
 
-
+	def pad!(size, value = nil)
+    	(size - self.length).times { self.push value }
+    	self.replace(self)
+	end
+end
 
 
 # 3. Refactored Solution
 
-
+# I'm sure something is inefficient there, I just can't see it at this point.
 
 # 4. Reflection 
+
+# This one didn't give me to much trouble beyond pad modifying the array that I was passing into it.  I couldn't figure out
+# why it was doing this as I assigning it to a new array and then modifying the new array before returning it.  With a little
+# research I realized that I had to use the .dup method to make this work correctly, I'm still not 100% why I had to do this to fix
+# the destruction issue, but I will have to research and find out!  I think it has something to do with the self reference
--- week_4/7_refactor/reverse_cipher/my_solution.rb
+++ week_4/7_refactor/reverse_cipher/my_solution.rb
@@ -10,36 +10,36 @@
 # Also make sure each step is necessary. If you don't think it's necessary
 # Try implementing the code without it. 
 
-def translate_to_cipher(sentence)
-    alphabet = ('a'..'z').to_a
-    cipher = Hash[alphabet.zip(alphabet.rotate(4))]
-    spaces = ["@", "#", "$", "%", "^", "&", "*"]
+def translate_to_cipher(sentence)  #Defining a method to pass a string agurment into
+    alphabet = ('a'..'z').to_a  #creating an array with individual letters as string elements
+    cipher = Hash[alphabet.zip(alphabet.rotate(4))]  #generating a hash using alphabet as the key, and the elements of alphabet rotated 4 spots to be the value.
+    spaces = ["@", "#", "$", "%", "^", "&", "*"] #generating and array of symbols as string elements.
     
-    original_sentence = sentence.downcase
-    encoded_sentence = []
-    original_sentence.each_char do |element|
-      if cipher.include?(element)
-        encoded_sentence << cipher[element]
-      elsif element == ' '
-        encoded_sentence << spaces.sample
+    original_sentence = sentence.downcase  #making sure the input is all lowercase
+    encoded_sentence = [] #generating a new array to hold the new string
+    original_sentence.each_char do |element|  #calling on each element to be passed into the method
+      if cipher.include?(element)  #checking to see if the element is a key in the cipher hash
+        encoded_sentence << cipher[element]  #if so, its value is placed in the new aray
+      elsif element == ' ' #checking to see if there is a space in the string
+        encoded_sentence << spaces.sample #if so, it places one of the spaces array elements into the new array
       else 
-        encoded_sentence << element
+        encoded_sentence << element #otherwise, it puts whatever element is there into the new array
       end
      end
     
-    return encoded_sentence.join
+    return encoded_sentence.join  # joins the elements of the new array into a string.
 end
 
 
 # Questions:
-# 1. What is the .to_a method doing?
-# 2. How does the rotate method work? What does it work on?
-# 3. What is `each_char` doing?
-# 4. What does `sample` do?
-# 5. Are there any other methods you want to understand better?
+# 1. What is the .to_a method doing?  It taking the elements of 'a'..'z' and adding them to the alphabet array
+# 2. How does the rotate method work? What does it work on?  rotate rotates the indexs of the array its called on by the value of the agrument that you pass it.
+# 3. What is `each_char` doing?  it's interating through the passed argument character by character
+# 4. What does `sample` do?  it randomly chooses an element of the array that it is called upon.
+# 5. Are there any other methods you want to understand better?  More than I can list ehre.
 # 6. Does this code look better or worse than your refactored solution
-#    of the original cipher code? What's better? What's worse?
-# 7. Is this good code? What makes it good? What makes it bad?
+#    of the original cipher code? What's better? What's worse?  We could have used the .each_car method instead of splitting the input into a new array.  We could also have used the rotate method.  Neither of which, however, we knew existed before this exercise!  Learning!
+# 7. Is this good code? What makes it good? What makes it bad?  It is good to the extent of how clean it is.  There is not much repetition going on at all.
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
@@ -55,3 +55,6 @@ p translate_to_cipher("I want cookies")
 
 # 5. Reflection 
 
+# everytime I think that I have some clear and concise, I learn of another method that could of made it the coding even simpler.
+# I suppose that at some point, I will know enough where that won't happen EVERY single exercise.  It's nice to learn new
+# things by examining someone elses code when you already know what its supposed to do, and that it works.
