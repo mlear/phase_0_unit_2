caa0a27ba7c508adaa23340e93e22da398490ad1
Martin Lear <m.lear09@gmail.com>
2014-04-26 16:35:06 -0500

    Group Project Update

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -5,12 +5,13 @@ my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4,
             "Annabelle" => 0, "Ditto" => 3}
 
 # Person 1's solution
+
 def my_array_finding_method(source, thing_to_find)
-  # Your code here!
+  source.select {|x| x.to_s.match(thing_to_find)}
 end
 
 def my_hash_finding_method(source, thing_to_find)
-  # Your code here!
+  (source.select { |k,v| v == thing_to_find }).keys
 end
 
 # Identify and describe the ruby method you implemented. 
