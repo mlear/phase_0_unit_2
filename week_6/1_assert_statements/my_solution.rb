c10b665d54d2997a1a0cee6e3b5615c031677b15
Martin Lear <m.lear09@gmail.com>
2014-05-08 20:50:06 -0500

    Updates

--- week_6/1_assert_statements/my_solution.rb
+++ week_6/1_assert_statements/my_solution.rb
@@ -11,21 +11,61 @@ end
 
 name = "bettysue"
 assert { name == "bettysue" }
-assert { name == "billybob" }
+#assert { name == "billybob" }
 
 # 2. Pseudocode what happens when the code above runs
 
-
+# => bettysue runes through fine because the test was true and the method yielded.  
+# => But when name was asked if it was equal to billybob, the assert method did not yeild so it rasied the "assertion failed"
+# => string on line 14.
 
 # 3. Copy your selected challenge here
 
+class CreditCard
 
+  def initialize(card)
+    @card = card.to_s.chars.map(&:to_i)
+    
+    raise ArgumentError.new ("Invalid input, not enough numbers!") unless @card.length == 16
+    
+  end
+  
+  def check_card
+    i = @card.length - 2
+
+    while i >= 0
+      @card[i] *= 2 
+      i -= 2
+    end
+
+    @card = @card.join.to_s.chars.map(&:to_i)
+                                  
+    @card.reduce(:+) % 10 == 0 ? true : false
+  end
+end
+
+card = CreditCard.new(4408041234567893)
+p card.check_card == true
+another_card = CreditCard.new(4408041234567892)
+p another_card.check_card == false
 
 
 # 4. Convert your driver test code from that challenge into Assert Statements
+card = CreditCard.new(4408041234567893)
+another_card = CreditCard.new(4408041234567892)
 
 
 
+def assert
+	raise "Assertion failed" unless yield
+end
+
+assert{ card.check_card == true }
+assert{ another_card.check_card == false }
 
 
 # 5. Reflection
+
+# => I'm a little boggled by this, but I'm sure it will settle in once I use it a few times, but as of right now I just don't
+# => don't get why the assert is better than a puts boolean statement?  It seems like that is more efficient..   or i am
+# => completely missing something really important.
--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,62 +1,68 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # 2. Original Code
 
 class Drawer
 
-attr_reader :contents
+  attr_reader :contents
 
-# Are there any more methods needed in this class?
+  # Are there any more methods needed in this class?
 
-def initialize
-@contents = []
-@open = true
+  def initialize
+    @contents = []
+    @open = true
+  end
+
+  def open
+    @open = true
+  end
+
+  def close
+    @open = false
+  end 
+
+  def add_item(item)
+    @contents << item
+  end
+
+  def remove_item(item = @contents.pop) #what is `#pop` doing?
+    @contents.delete(item)
+  end
+
+  def dump  # what should this method return?
+    @contents = []
+    puts "Your drawer is empty."
+  end
+
+  def view_contents
+    puts "The drawer contains:"
+    @contents.each {|silverware| puts "- " + silverware.type }
+  end
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
-attr_reader :type
+  attr_reader :type
 
-# Are there any more methods needed in this class?
+  # Are there any more methods needed in this class?
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
+  def initialize(type, clean = true)
+    @type = type
+    @clean = clean
+  end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
+  def eat
+    puts "eating with the #{type}"
+    @clean = false
+  end
+  
+  def clean_silverware
+    @clean = true
+    return "#{type} is clean!"
+  end
 
 end
 
@@ -85,18 +91,28 @@ silverware_drawer.view_contents
 silverware_drawer.dump
 silverware_drawer.view_contents #What should this return?
 
+pointy_fork = Silverware.new("pointy_fork")
+silverware_drawer.add_item(pointy_fork) 
+silverware_drawer.view_contents
+used_fork = silverware_drawer.remove_item(pointy_fork) #add some puts statements to help you trace through the code...
+used_fork.eat
 
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
-fork.eat
-
-#BONUS SECTION
-# puts fork.clean
+puts used_fork.clean_silverware
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+def assert
+  raise "Clean your silverware!" unless yield
+end
+
+assert { pointy_fork.instance_variable_get(:@clean) == true }
+assert { removed_knife.instance_variable_get(:@clean) == true }
 
 
 
 
-
-# 5. Reflection 
+# 5. Reflection:  This was a fun exercise, I really enjoy debugging and finding out whats
+# wrong with code.  I hope to do more of this in the future! (I'm sure I will.)
+# I'm just not sure I followed the spirit of the exercise when I started changing variable
+# names as fork was not being treated as a variable but as another method call. I think there
+# is a way to signify that we want fork to be variable and I can't remember or find it.
